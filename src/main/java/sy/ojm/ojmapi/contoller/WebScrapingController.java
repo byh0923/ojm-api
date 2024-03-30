84c927ad36f2d73785f04eadea44d172f1e4cf07
shkim <skok1025@naver.com>
2024-03-30 23:21:31 +0900

    cors 설정

--- src/main/java/sy/ojm/ojmapi/contoller/WebScrapingController.java
+++ src/main/java/sy/ojm/ojmapi/contoller/WebScrapingController.java
@@ -1,15 +1,13 @@
 package sy.ojm.ojmapi.contoller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import sy.ojm.ojmapi.dto.FeedbackDTO;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping(("/api/scape"))
 public class WebScrapingController {
